322ff6b5eb55001220f9aad7cc1a8b2b76f8ad0d
sergproua <spro@bitbucket>
2022-01-08 11:41:36 -0500

    allow for coordinate mutation

--- src/main/java/org/jfree/chart/annotations/XYBoxAnnotation.java
+++ src/main/java/org/jfree/chart/annotations/XYBoxAnnotation.java
@@ -373,4 +373,20 @@ public class XYBoxAnnotation extends AbstractXYAnnotation
         this.fillPaint = SerialUtils.readPaint(stream);
     }
 
+    /**
+     * Change coordinates and fire event
+     *
+     * @param x0
+     * @param y0
+     * @param x1
+     * @param y1
+     */
+    public void setCoordinates(double x0, double y0, double x1, double y1) {
+        this.x0 = x0;
+        this.x1 = x1;
+        this.y0 = y0;
+        this.y1 = y1;
+        fireAnnotationChanged();
+    }
+
 }
